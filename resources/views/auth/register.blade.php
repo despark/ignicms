c170f6351750815d2c1e1cd7b5493027cd241733
Alexander Stoimenov <me@astoimenov.eu>
2016-06-07 12:33:46 +0300

    Removed icons from register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -31,22 +31,18 @@
 
                 <div class="form-group has-feedback">
                     <input type="text" class="form-control" placeholder="Full name" name="name" value="{{ old('name') }}"/>
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
                 </div>
 
                 <div class="form-group has-feedback">
                     <input type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}"/>
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                 </div>
 
                 <div class="form-group has-feedback">
                     <input type="password" class="form-control" placeholder="Password" name="password"/>
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                 </div>
 
                 <div class="form-group has-feedback">
                     <input type="password" class="form-control" placeholder="Retype password" name="password_confirmation"/>
-                    <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
                 </div>
 
                 <div class="row">
