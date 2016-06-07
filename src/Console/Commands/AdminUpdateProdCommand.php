2493947ee09d350109ce381d812156286d3b29cb
Alexander Stoimenov <me@astoimenov.eu>
2016-06-07 10:53:18 +0300

    Added command to run on production

---
+++ src/Console/Commands/AdminUpdateProdCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Despark\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class AdminUpdateProdCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'admin:prod';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Runs migrations and clearing compiled files on production.';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        exec('composer install --no-scripts --no-dev');
+        $this->call('clear-compiled');
+        $this->call('optimize');
+
+        $this->call('migrate', [
+            '--force' => true,
+        ]);
+
+        exec('composer dumpautoload -o');
+    }
+}
