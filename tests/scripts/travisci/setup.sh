ad66006bb4cf4fca92da02974306e85be84eb95c
Ludmil Simeonov <lsimeonov@despark.com>
2016-08-26 15:28:39 +0300

    Try to fix git commit clone travis.yml

--- tests/scripts/travisci/setup.sh
+++ tests/scripts/travisci/setup.sh
@@ -4,12 +4,11 @@ mysql -e 'create database IF NOT EXISTS '$DB_DATABASE';' -u root
 mysql -e "grant all privileges on *.* to '$DB_USERNAME'@'localhost' with grant option;" -u root
 
 if [ ! -f "laravel/composer.json" ]; then
-    printenv TRAVIS_COMMIT_RANGE
     rm -rf laravel
     composer create-project laravel/laravel
     cd laravel
     composer update
-    composer require $TRAVIS_REPO_SLUG:dev-master#$TRAVIS_COMMIT
+    composer require $TRAVIS_REPO_SLUG:$TRAVIS_BRANCH\#$TRAVIS_COMMIT
     if [[ -v PACKAGE_PROVIDER ]]; then
         echo "$(awk '/'\''providers'\''[^\n]*?\[/ { print; print "'$(sed -e 's/\s*//g' <<<${PACKAGE_PROVIDER})',"; next }1' \
             config/app.php)" > config/app.php
