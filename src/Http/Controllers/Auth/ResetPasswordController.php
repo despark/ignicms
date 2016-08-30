6d3982cbebd64f2a39e16d152112d0047c75e28a
Alexander Stoimenov <me@astoimenov.eu>
2016-08-30 12:35:48 +0300

    Changed controller namespace in auth controllers

--- src/Http/Controllers/Auth/ResetPasswordController.php
+++ src/Http/Controllers/Auth/ResetPasswordController.php
@@ -2,7 +2,7 @@
 
 namespace Despark\Cms\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use Despark\Cms\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
