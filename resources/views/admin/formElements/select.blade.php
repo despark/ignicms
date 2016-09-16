6c80197cd7bfcba1966cd4a78673505f3c39b64e
Ludmil Simeonov <lsimeonov@despark.com>
2016-09-16 15:24:42 +0300

    Update select field to allow additional classes.

--- resources/views/admin/formElements/select.blade.php
+++ resources/views/admin/formElements/select.blade.php
@@ -1,6 +1,6 @@
-<div class="form-group">
-    {!! Form::label($elementName, $options['label']) !!}
-    {!! Form::select($elementName, [null => 'Select '.$options['label']] + $record->{$options['selectOptions']}(), $record->{$fieldName}, ['class' => 'form-control']) !!}
+<div class="form-group {{ $errors->has($fieldName) ? 'has-error' : '' }}">
+    {!! Form::label($fieldName, $options['label']) !!}
+    {!! Form::select($fieldName, [null => 'Select '.$options['label']] + $record->{$options['selectOptions']}(), $record->{$fieldName}, ['class' => 'form-control '.array_get($options, 'additionalClass')]) !!}
     <div class="text-red">
         {{ join($errors->get($fieldName), '<br />') }}
     </div>
