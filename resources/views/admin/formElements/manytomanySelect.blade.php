43ccad5d7b0ff0ec29bdcb6f3ab5795bf87df8a1
Alexander Stoimenov <me@astoimenov.eu>
2016-10-19 16:12:26 +0300

    Added option for select2 ajax autocomplete

--- resources/views/admin/formElements/manytomanySelect.blade.php
+++ resources/views/admin/formElements/manytomanySelect.blade.php
@@ -1,10 +1,44 @@
 <div class="form-group {{ $errors->has($options['validateName']) ? 'has-error' : '' }}">
     {!! Form::label($elementName, $options['label']) !!}
-    {!! Form::select($elementName, $sourceModel->toOptionsArray(), $record->{$options['relationMethod']}->pluck($options['selectedKey'])->all(), [
-            'class' => 'form-control select2 '.array_get($options, "additionalClass"),
+    {!! Form::select($elementName, isset($options["ajaxRouteName"]) ? $record->{$options['relationMethod']}->pluck($options['relationTextField'], $options['selectedKey'])->all() : $sourceModel->toOptionsArray(), $record->{$options['relationMethod']}->pluck($options['selectedKey'])->all(), [
+            'class' => 'form-control '.(isset($options["ajaxRouteName"]) ? '' : 'select2').' '.array_get($options, "additionalClass"),
             'multiple' => 'multiple',
         ]) !!}
     <div class="text-red">
         {{ join($errors->get($options['validateName']), '<br />') }}
     </div>
 </div>
+
+@if (isset($options['ajaxRouteName']) and isset($options['additionalClass']))
+    @push('additionalScripts')
+        <script type="text/javascript">
+            $(".{{ $options['additionalClass'] }}").select2({
+                tags: true,
+                multiple: true,
+                minimumInputLength: 2,
+                ajax: {
+                    url: '{{ route($options['ajaxRouteName']) }}',
+                    dataType: "json",
+                    type: "GET",
+                    data: function (params) {
+                        var queryParameters = {
+                            term: params.term
+                        };
+
+                        return queryParameters;
+                    },
+                    processResults: function (data) {
+                        return {
+                            results: $.map(data, function (item) {
+                                return {
+                                    id: item.id,
+                                    text: item.text
+                                };
+                            })
+                        };
+                    }
+                }
+            });
+        </script>
+    @endpush
+@endif
