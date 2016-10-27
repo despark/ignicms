a33bffa6a9b36196ad4a8d5905d0a1bff927ddd2
Ludmil Simeonov <youseeucs@gmail.com>
2016-10-27 10:50:24 +0300

    Optional class for checkbox fields

--- resources/views/admin/formElements/checkbox.blade.php
+++ resources/views/admin/formElements/checkbox.blade.php
@@ -2,7 +2,9 @@
     <div class="checkbox">
         <label for="{{ $elementName }}">
             {!! Form::hidden($elementName, 0) !!}
-            {!! Form::checkbox($elementName, 1, ($record->$fieldName), ['id' => $elementName]) !!}
+            {!! Form::checkbox($elementName, 1, ($record->$fieldName), [
+            'id' => $elementName,
+            'class' => isset($options['class']) ? $options['class'] : null]) !!}
             {{ $options['label'] }}
 
         </label>
