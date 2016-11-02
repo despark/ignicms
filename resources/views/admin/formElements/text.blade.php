5cfbe796b02010d5eee9891699495841004abf2c
Ludmil Simeonov <lsimeonov@despark.com>
2016-11-02 10:44:22 +0200

    Allow attributes for text field

--- resources/views/admin/formElements/text.blade.php
+++ resources/views/admin/formElements/text.blade.php
@@ -1,10 +1,10 @@
 <div class="form-group {{ $errors->has($fieldName) ? 'has-error' : '' }}">
     {!! Form::label($elementName, $options['label']) !!}
-    {!! Form::text($elementName, $record->$fieldName, [
-	    'id' =>  $elementName,
-	    'class' => "form-control",
-	    'placeholder' => $options['label'],
-	] ) !!}
+    {!! Form::text($elementName, $record->$fieldName, array_merge([
+    'id' =>  $elementName,
+    'class' => "form-control",
+    'placeholder' => $options['label'],
+], array_get($options, 'attributes', []))) !!}
     <div class="text-red">
         {{ join($errors->get($fieldName), '<br />') }}
     </div>
