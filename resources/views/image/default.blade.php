cb42c20f1bfcb696b03446f8d7349ace21c7beb8
Anton Geshev <ageshev@despark.com>
2016-11-25 17:33:01 +0200

    Update default.blade.php

--- resources/views/image/default.blade.php
+++ resources/views/image/default.blade.php
@@ -1,2 +1,2 @@
-<img src="{{ asset($image->getOriginalImagePath($thumb)) }}" alt="{{ $image->alt }}" title="{{$image->title}}"
-     srcset="{{ asset($image->getOriginalImagePath($thumb)) }} 1x, {{ asset($image->getRetinaImagePath($thumb)) }} {{$image->retina_factor}}x">
+<img {!! $attributes !!} src="{{ asset($image->getOriginalImagePath($thumb)) }}" alt="{{ $image->alt }}" title="{{$image->title}}"
+       srcset="{{ asset($image->getOriginalImagePath($thumb)) }} 1x, {{ asset($image->getRetinaImagePath($thumb)) }} {{$image->retina_factor}}x">
