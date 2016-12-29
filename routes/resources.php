4c9ca282e18433c403e3345ddf07b74ab9c0212c
Alexander Stoimenov <me@astoimenov.eu>
2016-12-29 09:22:07 +0200

    Fix routes names

--- routes/resources.php
+++ routes/resources.php
@@ -1,19 +1,9 @@
 <?php
 
-Route::resource('users', 'Admin\UsersController', [
-    'names' => [
-        'index'   => 'admin.users.index',
-        'store'   => 'admin.users.store',
-        'create'  => 'admin.users.create',
-        'update'  => 'admin.users.update',
-        'show'    => 'admin.users.show',
-        'destroy' => 'admin.users.destroy',
-        'edit'    => 'admin.users.edit',
-    ],
-]);
-Route::resource('seo_pages', 'Admin\SeoPagesController');
-Route::resource('roles', 'Admin\RolesController');
-Route::resource('permissions', 'Admin\PermissionsController');
+Route::resource('user', 'Admin\UsersController');
+Route::resource('seo_page', 'Admin\SeoPagesController');
+Route::resource('role', 'Admin\RolesController');
+Route::resource('permission', 'Admin\PermissionsController');
 
 Route::get('profile/edit', ['uses' => 'Admin\ProfileController@edit', 'as' => 'admin.profile.edit']);
 Route::get('profile/update', ['uses' => 'Admin\ProfileController@update', 'as' => 'admin.profile.update']);
