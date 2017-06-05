c743dbfc389cd12a3615d31aa90f2cca14422895
Alexander Stoimenov <me@astoimenov.eu>
2017-06-05 12:28:51 +0300

    Fix wrong position of date widget

--- resources/views/admin/formElements/date.blade.php
+++ resources/views/admin/formElements/date.blade.php
@@ -1,10 +1,12 @@
-<div class="form-group datepicker {{ $errors->has($fieldName) ? 'has-error' : '' }}">
+<div class="form-group {{ $errors->has($fieldName) ? 'has-error' : '' }}" style="position: 'relative'">
     {!! Form::label($elementName, $options['label']) !!}
-    {!! Form::text($elementName, $record->$fieldName, [
-	    'id' =>  $elementName,
-	    'class' => "form-control",
-	    'placeholder' => $options['label'],
-	] ) !!}
+    <div class="datepicker input-group">
+        {!! Form::text($elementName, $record->$fieldName, [
+            'id' =>  $elementName,
+            'class' => "form-control",
+            'placeholder' => $options['label'],
+        ] ) !!}
+    </div>
     <div class="text-red">
         {{ join($errors->get($fieldName), '<br />') }}
     </div>
